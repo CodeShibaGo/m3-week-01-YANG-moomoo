4314b50d31d46ff1ab6157fb16b8c01887458e58
yang <yangjuliet890731@gmail.com>
2023-12-31 16:54:39 +0800

    find_capitals.py 完成

--- find_capitals.py
+++ find_capitals.py
@@ -1,2 +1,8 @@
 def find_capitals(word):
-    pass
+    find = ""
+
+    for i in range(len(word)):
+        if word[i].islower() == False and word[i] != " ":
+            find += word[i]
+
+    return find.strip()
