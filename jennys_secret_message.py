888e538c3848ad367605af722a9124bea3ff089e
yang <yangjuliet890731@gmail.com>
2024-01-05 04:04:14 +0800

    jennys_secret_message.py 修改完成

--- jennys_secret_message.py
+++ jennys_secret_message.py
@@ -1,4 +1,8 @@
 def greet(name):
+
+    if not isinstance(name, str) or name == "":
+        return "Error: Name must non-empty"
+    
     if name == "Johnny":
         return "Hello, my love!"
     return "Hello, "+ name + "!"
