a8409f78a27e4b1c6eea5443d3bd84d1a456b0f7
yang <yangjuliet890731@gmail.com>
2024-01-05 03:53:43 +0800

    change_case.py 修改完成

--- change_case.py
+++ change_case.py
@@ -1,5 +1,7 @@
 def change_case(input_str, case):
     if case == "upper":
         return input_str.upper()
-    else:
+    elif case == "lower":
         return input_str.lower()
+    else:
+        return "Error: Unknown case parameter"
